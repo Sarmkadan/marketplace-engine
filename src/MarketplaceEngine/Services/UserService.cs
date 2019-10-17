9ff211badf322ebdf2b4ca64bd33858304787a1b
Vladyslav Zaiets <rutova2@gmail.com>
2019-10-17 07:33:21 +0300

    Nits

---
+++ src/MarketplaceEngine/Services/UserService.cs
@@ -0,0 +1,227 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using MarketplaceEngine.Domain.Enums;
+using MarketplaceEngine.Domain.Models;
+using MarketplaceEngine.Domain.ValueObjects;
+using MarketplaceEngine.Exceptions;
+using MarketplaceEngine.Repositories;
+
+namespace MarketplaceEngine.Services;
+
+/// <summary>
+/// Service for managing user accounts and profiles.
+/// </summary>
+public class UserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+    }
+
+    // Creates a new user account
+    public async Task<User> RegisterUserAsync(string email, string fullName, string? phone = null)
+    {
+        var existingUser = await _userRepository.GetByEmailAsync(email);
+        if (existingUser != null)
+            throw new DuplicateResourceException("User", "email", email);
+
+        var user = new User
+        {
+            Email = email,
+            FullName = fullName,
+            Phone = phone,
+            Role = UserRole.User,
+            IsActive = true,
+            IsVerified = false
+        };
+
+        user.ValidateProfile();
+        user.GenerateVerificationToken();
+
+        return await _userRepository.AddAsync(user);
+    }
+
+    // Gets user by ID
+    public async Task<User> GetUserAsync(Guid userId)
+    {
+        var user = await _userRepository.GetByIdAsync(userId);
+        if (user == null)
+            throw new ResourceNotFoundException("User", userId);
+
+        return user;
+    }
+
+    // Gets user by email
+    public async Task<User> GetUserByEmailAsync(string email)
+    {
+        var user = await _userRepository.GetByEmailAsync(email);
+        if (user == null)
+            throw new ResourceNotFoundException("User", email);
+
+        return user;
+    }
+
+    // Updates user profile
+    public async Task<User> UpdateProfileAsync(Guid userId, string? fullName = null, string? phone = null,
+        string? bio = null, Location? location = null)
+    {
+        var user = await GetUserAsync(userId);
+
+        if (!string.IsNullOrEmpty(fullName))
+            user.FullName = fullName;
+
+        if (phone != null)
+            user.Phone = string.IsNullOrWhiteSpace(phone) ? null : phone;
+
+        if (bio != null)
+            user.Bio = string.IsNullOrWhiteSpace(bio) ? null : bio;
+
+        if (location != null)
+            user.Location = location;
+
+        user.ValidateProfile();
+        return await _userRepository.UpdateAsync(user);
+    }
+
+    // Verifies user email
+    public async Task<bool> VerifyEmailAsync(Guid userId, string verificationToken)
+    {
+        var user = await GetUserAsync(userId);
+
+        if (user.VerifyEmail(verificationToken))
+        {
+            await _userRepository.UpdateAsync(user);
+            return true;
+        }
+
+        return false;
+    }
+
+    // Resends verification email
+    public async Task<User> ResendVerificationTokenAsync(string email)
+    {
+        var user = await _userRepository.GetByEmailAsync(email);
+        if (user == null)
+            throw new ResourceNotFoundException("User", email);
+
+        if (user.IsVerified)
+            throw new InvalidOperationException("User email is already verified");
+
+        user.GenerateVerificationToken();
+        return await _userRepository.UpdateAsync(user);
+    }
+
+    // Promotes user to premium seller
+    public async Task<User> PromoteToPremiumAsync(Guid userId)
+    {
+        var user = await GetUserAsync(userId);
+
+        if (user.TotalSales < 5)
+            throw new InvalidOperationException("User must have at least 5 sales to be promoted to premium");
+
+        if (user.Rating == null || user.Rating.Score < 4)
+            throw new InvalidOperationException("User must have a rating of 4+ stars to be promoted to premium");
+
+        user.PromoteToPremiumSeller();
+        return await _userRepository.UpdateAsync(user);
+    }
+
+    // Deactivates user account
+    public async Task<User> DeactivateAccountAsync(Guid userId)
+    {
+        var user = await GetUserAsync(userId);
+        user.Deactivate();
+        return await _userRepository.UpdateAsync(user);
+    }
+
+    // Reactivates user account
+    public async Task<User> ReactivateAccountAsync(Guid userId)
+    {
+        var user = await GetUserAsync(userId);
+
+        if (!user.IsActive)
+        {
+            user.IsActive = true;
+            user.UpdatedAt = DateTime.UtcNow;
+            return await _userRepository.UpdateAsync(user);
+        }
+
+        return user;
+    }
+
+    // Records a sale for the user
+    public async Task<User> RecordSaleAsync(Guid userId)
+    {
+        var user = await GetUserAsync(userId);
+        user.RecordSale();
+        return await _userRepository.UpdateAsync(user);
+    }
+
+    // Updates user rating
+    public async Task<User> UpdateRatingAsync(Guid userId, Rating rating)
+    {
+        var user = await GetUserAsync(userId);
+        user.UpdateRating(rating);
+        return await _userRepository.UpdateAsync(user);
+    }
+
+    // Gets top sellers
+    public async Task<List<User>> GetTopSellersAsync(int limit = 10)
+    {
+        if (limit < 1 || limit > 50)
+            limit = 10;
+
+        return await _userRepository.GetTopSellersAsync(limit);
+    }
+
+    // Gets paginated users
+    public async Task<(List<User> items, int total)> GetPaginatedUsersAsync(int pageNumber, int pageSize)
+    {
+        return await _userRepository.GetPagedAsync(pageNumber, pageSize);
+    }
+
+    // Updates last activity timestamp
+    public async Task UpdateLastActivityAsync(Guid userId)
+    {
+        await _userRepository.UpdateLastActivityAsync(userId);
+    }
+
+    // Gets verified users count
+    public async Task<int> GetVerifiedUserCountAsync()
+    {
+        var verified = await _userRepository.GetVerifiedUsersAsync();
+        return verified.Count;
+    }
+
+    // Gets active users count
+    public async Task<int> GetActiveUserCountAsync()
+    {
+        var active = await _userRepository.GetActiveUsersAsync();
+        return active.Count;
+    }
+
+    // Validates user can perform action
+    public async Task ValidateUserAccessAsync(Guid userId)
+    {
+        var user = await GetUserAsync(userId);
+
+        if (!user.IsActive)
+            throw new UnauthorizedException(userId, "access the marketplace");
+
+        if (!user.IsVerified)
+            throw new UnauthorizedException(userId, "perform this action - email not verified");
+    }
+
+    // Gets user profile for public display
+    public async Task<User> GetPublicProfileAsync(Guid userId)
+    {
+        var user = await GetUserAsync(userId);
+        await _userRepository.UpdateLastActivityAsync(userId);
+        return user;
+    }
+}
---
+++ src/MarketplaceEngine/Utilities/ValidationUtility.cs
@@ -0,0 +1,131 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.RegularExpressions;
+
+namespace MarketplaceEngine.Utilities;
+
+/// <summary>
+/// Centralized validation utilities for common data validation scenarios.
+/// Ensures consistent validation rules across the application.
+/// </summary>
+public static class ValidationUtility
+{
+    private static readonly Regex EmailRegex = new(
+        @"^[^\s@]+@[^\s@]+\.[^\s@]+$",
+        RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+    private static readonly Regex PhoneRegex = new(
+        @"^\+?[1-9]\d{1,14}$",
+        RegexOptions.Compiled);
+
+    /// <summary>
+    /// Validates email address format.
+    /// </summary>
+    public static bool IsValidEmail(string? email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            return false;
+
+        return EmailRegex.IsMatch(email) && email.Length <= 254;
+    }
+
+    /// <summary>
+    /// Validates phone number format (international format).
+    /// </summary>
+    public static bool IsValidPhoneNumber(string? phone)
+    {
+        if (string.IsNullOrWhiteSpace(phone))
+            return false;
+
+        return PhoneRegex.IsMatch(phone);
+    }
+
+    /// <summary>
+    /// Validates that a string is not empty and within length constraints.
+    /// </summary>
+    public static bool IsValidText(string? text, int minLength = 1, int maxLength = int.MaxValue)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+            return false;
+
+        return text.Length >= minLength && text.Length <= maxLength;
+    }
+
+    /// <summary>
+    /// Validates a URL format.
+    /// </summary>
+    public static bool IsValidUrl(string? url)
+    {
+        if (string.IsNullOrWhiteSpace(url))
+            return false;
+
+        return Uri.TryCreate(url, UriKind.Absolute, out var uriResult) &&
+               (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+    }
+
+    /// <summary>
+    /// Validates a price is positive and within reasonable bounds.
+    /// </summary>
+    public static bool IsValidPrice(decimal price, decimal minPrice = 0.01m, decimal maxPrice = 999999.99m)
+    {
+        return price >= minPrice && price <= maxPrice;
+    }
+
+    /// <summary>
+    /// Validates a rating score (typically 1-5 stars).
+    /// </summary>
+    public static bool IsValidRating(decimal rating, decimal minRating = 0m, decimal maxRating = 5m)
+    {
+        return rating >= minRating && rating <= maxRating;
+    }
+
+    /// <summary>
+    /// Validates a GUID is not empty.
+    /// </summary>
+    public static bool IsValidGuid(Guid guid)
+    {
+        return guid != Guid.Empty;
+    }
+
+    /// <summary>
+    /// Validates pagination parameters.
+    /// </summary>
+    public static bool IsValidPagination(int page, int pageSize, int maxPageSize = 100)
+    {
+        return page >= 1 && pageSize >= 1 && pageSize <= maxPageSize;
+    }
+
+    /// <summary>
+    /// Sanitizes user input to prevent injection attacks.
+    /// Removes potentially harmful characters.
+    /// </summary>
+    public static string SanitizeInput(string input, int maxLength = 1000)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return string.Empty;
+
+        // Remove null characters and control characters
+        var sanitized = new string(input
+            .Where(c => !char.IsControl(c) || c == '\n' || c == '\r' || c == '\t')
+            .ToArray());
+
+        // Truncate to max length
+        return sanitized.Length > maxLength
+            ? sanitized.Substring(0, maxLength)
+            : sanitized;
+    }
+
+    /// <summary>
+    /// Validates a search query is appropriate length and not empty.
+    /// </summary>
+    public static bool IsValidSearchQuery(string? query, int minLength = 2, int maxLength = 100)
+    {
+        if (string.IsNullOrWhiteSpace(query))
+            return false;
+
+        return query.Length >= minLength && query.Length <= maxLength;
+    }
+}
