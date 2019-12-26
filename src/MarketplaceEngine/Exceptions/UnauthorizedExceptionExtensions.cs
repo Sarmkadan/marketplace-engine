e8fd7650277a9a877ddca4ae91f0f4af9882b5ff
Vladyslav Zaiets <rutova2@gmail.com>
2019-12-26 08:12:48 +0200

    Extension methods on UnauthorizedException

---
+++ src/MarketplaceEngine/Exceptions/UnauthorizedExceptionExtensions.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace MarketplaceEngine.Exceptions
+{
+    /// <summary>
+    /// Extension methods for <see cref="UnauthorizedException"/>.
+    /// </summary>
+    public static class UnauthorizedExceptionExtensions
+    {
+        /// <summary>
+        /// Generates a concise log message that includes the user identifier and the attempted action.
+        /// </summary>
+        /// <param name="exception">The <see cref="UnauthorizedException"/> instance.</param>
+        /// <returns>A formatted string suitable for logging.</returns>
+        public static string ToLogMessage(this UnauthorizedException exception)
+        {
+            return $"Unauthorized access: UserId={exception.UserId}, Action=\"{exception.Action}\".";
+        }
+
+        /// <summary>
+        /// Determines whether the exception pertains to the specified user.
+        /// </summary>
+        /// <param name="exception">The <see cref="UnauthorizedException"/> instance.</param>
+        /// <param name="userId">The user identifier to compare against.</param>
+        /// <returns><c>true</c> if the exception's <c>UserId</c> matches <paramref name="userId"/>; otherwise, <c>false</c>.</returns>
+        public static bool IsForUser(this UnauthorizedException exception, Guid userId)
+        {
+            return exception.UserId == userId;
+        }
+
+        /// <summary>
+        /// Returns a human‑readable description of the unauthorized action.
+        /// </summary>
+        /// <param name="exception">The <see cref="UnauthorizedException"/> instance.</param>
+        /// <returns>A string describing the attempted action.</returns>
+        public static string GetActionDescription(this UnauthorizedException exception)
+        {
+            return $"User attempted unauthorized action: \"{exception.Action}\".";
+        }
+
+        /// <summary>
+        /// Throws the exception if its <c>UserId</c> does not match the expected identifier.
+        /// </summary>
+        /// <param name="exception">The <see cref="UnauthorizedException"/> instance.</param>
+        /// <param name="expectedUserId">The user identifier that is expected.</param>
+        public static void ThrowIfUserMismatch(this UnauthorizedException exception, Guid expectedUserId)
+        {
+            if (exception.UserId != expectedUserId)
+            {
+                throw exception;
+            }
+        }
+    }
+}
