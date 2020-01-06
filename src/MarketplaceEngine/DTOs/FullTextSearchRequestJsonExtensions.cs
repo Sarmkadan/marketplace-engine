03af10693dfd215c3acee45890c2347c851b8464
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-06 20:14:22 +0200

    Fix

---
+++ src/MarketplaceEngine/DTOs/FullTextSearchRequestJsonExtensions.cs
@@ -0,0 +1,93 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MarketplaceEngine.DTOs;
+
+/// <summary>
+/// Provides System.Text.Json serialization helpers for <see cref="FullTextSearchRequest"/>.
+/// </summary>
+public static class FullTextSearchRequestJsonExtensions
+{
+    private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        WriteIndented = false
+    };
+
+    private static readonly JsonSerializerOptions _indentedOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        WriteIndented = true
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="FullTextSearchRequest"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this FullTextSearchRequest value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        return JsonSerializer.Serialize(value, indented ? _indentedOptions : _options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="FullTextSearchRequest"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized instance, or <see langword="null"/> if the JSON is empty.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static FullTextSearchRequest? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<FullTextSearchRequest>(json, _options);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="FullTextSearchRequest"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+    public static bool TryFromJson(string json, out FullTextSearchRequest? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<FullTextSearchRequest>(json, _options);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
