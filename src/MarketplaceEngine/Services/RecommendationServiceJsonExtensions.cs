b0dda8c54bea6f0d89fda63b85279bb7aacb79c9
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-16 12:50:20 +0200

    Json serialization for RecommendationServiceJson

---
+++ src/MarketplaceEngine/Services/RecommendationServiceJsonExtensions.cs
@@ -0,0 +1,83 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace MarketplaceEngine.Services;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="RecommendationService"/>.
+/// </summary>
+/// <remarks>
+/// These extension methods enable round-trip serialization of <see cref="RecommendationService"/> instances
+/// to JSON and back, using camelCase property naming and invariant culture formatting.
+/// </remarks>
+public static class RecommendationServiceJsonExtensions
+{
+    private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="RecommendationService"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The service instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON representation of the service instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this RecommendationService value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_options) { WriteIndented = true }
+            : _options;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="RecommendationService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized service instance, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static RecommendationService? FromJson(string json)
+    {
+        if (string.IsNullOrWhiteSpace(json))
+            return null;
+
+        return JsonSerializer.Deserialize<RecommendationService>(json, _options);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="RecommendationService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized service instance, or null if deserialization fails.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out RecommendationService? value)
+    {
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+            return false;
+
+        try
+        {
+            value = JsonSerializer.Deserialize<RecommendationService>(json, _options);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
