3261f792567405949eab5267f5751720e6a1e62e
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-21 13:30:07 +0200

    Add DateTimeUtilityJson json helpers
    will circle back to tests later.

---
+++ src/MarketplaceEngine/Utilities/DateTimeUtilityJsonExtensions.cs
@@ -0,0 +1,111 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MarketplaceEngine.Utilities;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for DateTime values.
+/// Enables consistent JSON handling of DateTime values across the application.
+/// </summary>
+public static class DateTimeUtilityJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        WriteIndented = false,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new UtcDateTimeConverter() }
+    };
+
+    /// <summary>
+    /// Serializes a DateTime value to a JSON string.
+    /// </summary>
+    /// <param name="value">The DateTime value to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the DateTime value.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this DateTime value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a DateTime value.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized DateTime value, or null if the JSON is null or empty.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static DateTime? FromJson(string json)
+    {
+        if (string.IsNullOrEmpty(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<DateTime>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a DateTime value.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized DateTime value if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out DateTime? value)
+    {
+        value = null;
+
+        if (string.IsNullOrEmpty(json))
+        {
+            return true;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<DateTime>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Custom JSON converter to ensure DateTime values are always treated as UTC.
+    /// </summary>
+    private sealed class UtcDateTimeConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Null)
+            {
+                return default;
+            }
+
+            var dateTime = JsonSerializer.Deserialize<DateTime>(ref reader, options);
+            return DateTimeUtility.ToUtc(dateTime);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            var utcValue = DateTimeUtility.ToUtc(value);
+            JsonSerializer.Serialize(writer, utcValue, options);
+        }
+    }
+}
