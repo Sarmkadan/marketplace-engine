81067ab6f825c481e04152446af56a391bff4e33
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-22 09:11:34 +0200

    serialize MessagesControllerJson properly

---
+++ src/MarketplaceEngine/Controllers/MessagesControllerJsonExtensions.cs
@@ -0,0 +1,82 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MarketplaceEngine.Controllers;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="MessagesController"/>.
+/// </summary>
+public static class MessagesControllerJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="MessagesController"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The controller instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the controller.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    public static string ToJson(this MessagesController value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonSerializerOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonSerializerOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="MessagesController"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized <see cref="MessagesController"/> instance, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is null or empty.</exception>
+    public static MessagesController? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        return JsonSerializer.Deserialize<MessagesController>(json, _jsonSerializerOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="MessagesController"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is null or empty.</exception>
+    public static bool TryFromJson(string json, out MessagesController? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<MessagesController>(json, _jsonSerializerOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
