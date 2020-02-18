5eaa74bdc87dcff87ba26be48d1e91b0bbe8706c
Vladyslav Zaiets <rutova2@gmail.com>
2020-02-18 18:47:36 +0200

    WebhookvEent extensions
    mostly WebhookEventExtensions.cs, nothing exciting.

--- src/MarketplaceEngine/Infrastructure/Integration/WebhookEventExtensions.cs
+++ src/MarketplaceEngine/Infrastructure/Integration/WebhookEventExtensions.cs
@@ -1,22 +1,28 @@
+using System.Globalization;
 using System.Text.Json;
 
 namespace MarketplaceEngine.Infrastructure.Integration;
 
 /// <summary>
-/// Extension methods for WebhookEvent providing common webhook processing utilities.
+/// Extension methods for <see cref="WebhookEvent"/> providing common webhook processing utilities.
 /// </summary>
 public static class WebhookEventExtensions
 {
     /// <summary>
     /// Safely gets a strongly-typed value from the webhook data dictionary.
-    /// Returns null if the key doesn't exist or the value is null.
+    /// Returns null if the webhook event, key, or value is null, or if deserialization fails.
     /// </summary>
     /// <typeparam name="T">The type to deserialize to</typeparam>
-    /// <param name="webhookEvent">The webhook event</param>
-    /// <param name="key">The key in the Data dictionary</param>
-    /// <returns>The deserialized value or null</returns>
+    /// <param name="webhookEvent">The webhook event.</param>
+    /// <param name="key">The key in the Data dictionary.</param>
+    /// <returns>The deserialized value or null.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="webhookEvent"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="key"/> is null.</exception>
     public static T? GetData<T>(this WebhookEvent webhookEvent, string key)
     {
+        ArgumentNullException.ThrowIfNull(webhookEvent);
+        ArgumentNullException.ThrowIfNull(key);
+
         if (webhookEvent.Data.TryGetValue(key, out var value) && value is not null)
         {
             try
@@ -38,41 +44,45 @@ public static class WebhookEventExtensions
 
     /// <summary>
     /// Safely gets a string value from the webhook data dictionary.
-    /// Returns null if the key doesn't exist or the value is null.
+    /// Returns null if the webhook event, key, or value is null.
     /// </summary>
-    /// <param name="webhookEvent">The webhook event</param>
-    /// <param name="key">The key in the Data dictionary</param>
-    /// <returns>The string value or null</returns>
+    /// <param name="webhookEvent">The webhook event.</param>
+    /// <param name="key">The key in the Data dictionary.</param>
+    /// <returns>The string value or null.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="webhookEvent"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="key"/> is null.</exception>
     public static string? GetDataString(this WebhookEvent webhookEvent, string key)
     {
-        if (webhookEvent.Data.TryGetValue(key, out var value) && value is not null)
-        {
-            return value.ToString();
-        }
+        ArgumentNullException.ThrowIfNull(webhookEvent);
+        ArgumentNullException.ThrowIfNull(key);
 
-        return null;
+        return webhookEvent.Data.TryGetValue(key, out var value) && value is not null
+            ? value.ToString()
+            : null;
     }
 
     /// <summary>
     /// Safely gets a numeric value from the webhook data dictionary.
-    /// Returns null if the key doesn't exist, the value is null, or conversion fails.
+    /// Returns null if the webhook event, key, or value is null, or if conversion fails.
     /// </summary>
-    /// <param name="webhookEvent">The webhook event</param>
-    /// <param name="key">The key in the Data dictionary</param>
-    /// <returns>The decimal value or null</returns>
+    /// <param name="webhookEvent">The webhook event.</param>
+    /// <param name="key">The key in the Data dictionary.</param>
+    /// <returns>The decimal value or null.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="webhookEvent"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="key"/> is null.</exception>
     public static decimal? GetDataDecimal(this WebhookEvent webhookEvent, string key)
     {
+        ArgumentNullException.ThrowIfNull(webhookEvent);
+        ArgumentNullException.ThrowIfNull(key);
+
         if (webhookEvent.Data.TryGetValue(key, out var value) && value is not null)
         {
-            if (value is decimal decimalValue)
+            return value switch
             {
-                return decimalValue;
-            }
-
-            if (decimal.TryParse(value.ToString(), out var parsedValue))
-            {
-                return parsedValue;
-            }
+                decimal decimalValue => decimalValue,
+                string strValue => decimal.TryParse(strValue, CultureInfo.InvariantCulture, out var parsedValue) ? parsedValue : null,
+                _ => decimal.TryParse(value.ToString(), CultureInfo.InvariantCulture, out var parsedValue) ? parsedValue : null
+            };
         }
 
         return null;
@@ -80,24 +90,26 @@ public static class WebhookEventExtensions
 
     /// <summary>
     /// Safely gets a boolean value from the webhook data dictionary.
-    /// Returns null if the key doesn't exist, the value is null, or conversion fails.
+    /// Returns null if the webhook event, key, or value is null, or if conversion fails.
     /// </summary>
-    /// <param name="webhookEvent">The webhook event</param>
-    /// <param name="key">The key in the Data dictionary</param>
-    /// <returns>The boolean value or null</returns>
+    /// <param name="webhookEvent">The webhook event.</param>
+    /// <param name="key">The key in the Data dictionary.</param>
+    /// <returns>The boolean value or null.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="webhookEvent"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="key"/> is null.</exception>
     public static bool? GetDataBoolean(this WebhookEvent webhookEvent, string key)
     {
+        ArgumentNullException.ThrowIfNull(webhookEvent);
+        ArgumentNullException.ThrowIfNull(key);
+
         if (webhookEvent.Data.TryGetValue(key, out var value) && value is not null)
         {
-            if (value is bool boolValue)
+            return value switch
             {
-                return boolValue;
-            }
-
-            if (bool.TryParse(value.ToString(), out var parsedValue))
-            {
-                return parsedValue;
-            }
+                bool boolValue => boolValue,
+                string strValue => bool.TryParse(strValue, out var parsedValue) ? parsedValue : null,
+                _ => bool.TryParse(value.ToString(), out var parsedValue) ? parsedValue : null
+            };
         }
 
         return null;
