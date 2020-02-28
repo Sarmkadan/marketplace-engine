df52aa4109f131839dc564f39ceb22bcebe7f950
Vladyslav Zaiets <rutova2@gmail.com>
2020-02-28 07:52:30 +0200

    add extension helpers for UnauthorizedException
    also touched a couple of call sites.

--- src/MarketplaceEngine/Exceptions/UnauthorizedExceptionExtensions.cs
+++ src/MarketplaceEngine/Exceptions/UnauthorizedExceptionExtensions.cs
@@ -12,8 +12,10 @@ namespace MarketplaceEngine.Exceptions
         /// </summary>
         /// <param name="exception">The <see cref="UnauthorizedException"/> instance.</param>
         /// <returns>A formatted string suitable for logging.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
         public static string ToLogMessage(this UnauthorizedException exception)
         {
+            ArgumentNullException.ThrowIfNull(exception);
             return $"Unauthorized access: UserId={exception.UserId}, Action=\"{exception.Action}\".";
         }
 
@@ -23,8 +25,10 @@ namespace MarketplaceEngine.Exceptions
         /// <param name="exception">The <see cref="UnauthorizedException"/> instance.</param>
         /// <param name="userId">The user identifier to compare against.</param>
         /// <returns><c>true</c> if the exception's <c>UserId</c> matches <paramref name="userId"/>; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
         public static bool IsForUser(this UnauthorizedException exception, Guid userId)
         {
+            ArgumentNullException.ThrowIfNull(exception);
             return exception.UserId == userId;
         }
 
@@ -33,8 +37,10 @@ namespace MarketplaceEngine.Exceptions
         /// </summary>
         /// <param name="exception">The <see cref="UnauthorizedException"/> instance.</param>
         /// <returns>A string describing the attempted action.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
         public static string GetActionDescription(this UnauthorizedException exception)
         {
+            ArgumentNullException.ThrowIfNull(exception);
             return $"User attempted unauthorized action: \"{exception.Action}\".";
         }
 
@@ -43,8 +49,10 @@ namespace MarketplaceEngine.Exceptions
         /// </summary>
         /// <param name="exception">The <see cref="UnauthorizedException"/> instance.</param>
         /// <param name="expectedUserId">The user identifier that is expected.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/></exception>
         public static void ThrowIfUserMismatch(this UnauthorizedException exception, Guid expectedUserId)
         {
+            ArgumentNullException.ThrowIfNull(exception);
             if (exception.UserId != expectedUserId)
             {
                 throw exception;
