6b9e0f3d305791b6079f31ca31e92c472d1fee08
Vladyslav Zaiets <rutova2@gmail.com>
2020-03-03 17:23:43 +0200

    input validation for MarketplaceDbContext

--- src/MarketplaceEngine/Data/MarketplaceDbContextValidation.cs
+++ src/MarketplaceEngine/Data/MarketplaceDbContextValidation.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using MarketplaceEngine.Domain.Models;
+using System.Diagnostics.CodeAnalysis;
 
 namespace MarketplaceEngine.Data
 {
@@ -15,51 +16,21 @@ namespace MarketplaceEngine.Data
         /// </summary>
         /// <param name="value">The <see cref="MarketplaceDbContext"/> instance to validate.</param>
         /// <returns>A read‑only list of validation error messages. Empty if the instance is valid.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
         public static IReadOnlyList<string> Validate(this MarketplaceDbContext value)
         {
+            ArgumentNullException.ThrowIfNull(value);
+
             var problems = new List<string>();
 
-            if (value is null)
-            {
-                problems.Add("MarketplaceDbContext instance is null.");
-                return problems;
-            }
-
             // Collections must not be null
-            if (value.Users is null)
-                problems.Add("Users collection is null.");
-            else if (value.Users.Any(u => u is null))
-                problems.Add("Users collection contains null entries.");
-
-            if (value.Categories is null)
-                problems.Add("Categories collection is null.");
-            else if (value.Categories.Any(c => c is null))
-                problems.Add("Categories collection contains null entries.");
-
-            if (value.Listings is null)
-                problems.Add("Listings collection is null.");
-            else if (value.Listings.Any(l => l is null))
-                problems.Add("Listings collection contains null entries.");
-
-            if (value.Messages is null)
-                problems.Add("Messages collection is null.");
-            else if (value.Messages.Any(m => m is null))
-                problems.Add("Messages collection contains null entries.");
-
-            if (value.ModerationReports is null)
-                problems.Add("ModerationReports collection is null.");
-            else if (value.ModerationReports.Any(r => r is null))
-                problems.Add("ModerationReports collection contains null entries.");
-
-            if (value.Payments is null)
-                problems.Add("Payments collection is null.");
-            else if (value.Payments.Any(p => p is null))
-                problems.Add("Payments collection contains null entries.");
-
-            if (value.Reviews is null)
-                problems.Add("Reviews collection is null.");
-            else if (value.Reviews.Any(r => r is null))
-                problems.Add("Reviews collection contains null entries.");
+            problems.AddRange(ValidateCollection(value.Users, nameof(value.Users)));
+            problems.AddRange(ValidateCollection(value.Categories, nameof(value.Categories)));
+            problems.AddRange(ValidateCollection(value.Listings, nameof(value.Listings)));
+            problems.AddRange(ValidateCollection(value.Messages, nameof(value.Messages)));
+            problems.AddRange(ValidateCollection(value.ModerationReports, nameof(value.ModerationReports)));
+            problems.AddRange(ValidateCollection(value.Payments, nameof(value.Payments)));
+            problems.AddRange(ValidateCollection(value.Reviews, nameof(value.Reviews)));
 
             // Total entity count must be non‑negative and consistent
             int totalCount;
@@ -67,7 +38,9 @@ namespace MarketplaceEngine.Data
             {
                 totalCount = value.GetTotalEntityCount();
                 if (totalCount < 0)
+                {
                     problems.Add("Total entity count is negative.");
+                }
             }
             catch (Exception ex)
             {
@@ -76,35 +49,62 @@ namespace MarketplaceEngine.Data
             }
 
             // Verify that the total count matches the sum of the collections
-            int sum = (value.Users?.Count ?? 0) +
-                      (value.Categories?.Count ?? 0) +
-                      (value.Listings?.Count ?? 0) +
-                      (value.Messages?.Count ?? 0) +
-                      (value.ModerationReports?.Count ?? 0) +
-                      (value.Payments?.Count ?? 0) +
-                      (value.Reviews?.Count ?? 0);
+            int sum = (value.Users?.Count ?? 0)
+                + (value.Categories?.Count ?? 0)
+                + (value.Listings?.Count ?? 0)
+                + (value.Messages?.Count ?? 0)
+                + (value.ModerationReports?.Count ?? 0)
+                + (value.Payments?.Count ?? 0)
+                + (value.Reviews?.Count ?? 0);
 
             if (totalCount >= 0 && totalCount != sum)
-                problems.Add($"Total entity count ({totalCount}) does not match the sum of individual collections ({sum}).");
+            {
+                problems.Add(string.Format("Total entity count ({0}) does not match the sum of individual collections ({1}).", totalCount, sum));
+            }
 
             return problems.AsReadOnly();
         }
 
         /// <summary>
+        /// Validates a collection and returns validation messages if issues are found.
+        /// </summary>
+        /// <typeparam name="T">The collection element type.</typeparam>
+        /// <param name="collection">The collection to validate.</param>
+        /// <param name="collectionName">The name of the collection property.</param>
+        /// <returns>List of validation error messages, or empty list if valid.</returns>
+        private static IEnumerable<string> ValidateCollection<T>(IEnumerable<T>? collection, string collectionName)
+        {
+            if (collection is null)
+            {
+                yield return string.Format("Collection '{0}' is null.", collectionName);
+                yield break;
+            }
+
+            if (collection.Any(item => item is null))
+            {
+                yield return string.Format("Collection '{0}' contains null entries.", collectionName);
+            }
+        }
+
+        /// <summary>
         /// Returns <c>true</c> if the <paramref name="value"/> passes all validation checks.
         /// </summary>
+        /// <param name="value">The <see cref="MarketplaceDbContext"/> to check.</param>
+        /// <returns>True if valid; otherwise false.</returns>
         public static bool IsValid(this MarketplaceDbContext value) => !value.Validate().Any();
 
         /// <summary>
         /// Ensures the <paramref name="value"/> is valid, otherwise throws an <see cref="ArgumentException"/>.
         /// </summary>
+        /// <param name="value">The <see cref="MarketplaceDbContext"/> to validate.</param>
+        /// <exception cref="ArgumentException">Thrown when validation fails.</exception>
         public static void EnsureValid(this MarketplaceDbContext value)
         {
             var problems = value.Validate();
             if (problems.Any())
             {
                 throw new ArgumentException(
-                    $"MarketplaceDbContext validation failed: {string.Join("; ", problems)}",
+                    string.Format("MarketplaceDbContext validation failed: {0}", string.Join("; ", problems)),
                     nameof(value));
             }
         }
