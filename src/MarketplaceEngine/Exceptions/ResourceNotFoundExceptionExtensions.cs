f6ac5f5658b1784c4c88ff415eac451f68eab2ef
Vladyslav Zaiets <rutova2@gmail.com>
2020-03-06 20:32:45 +0200

    feat(exceptions): more ResourceNotFoundException extension methods
    will circle back to tests later.

--- src/MarketplaceEngine/Exceptions/ResourceNotFoundExceptionExtensions.cs
+++ src/MarketplaceEngine/Exceptions/ResourceNotFoundExceptionExtensions.cs
@@ -5,6 +5,10 @@ namespace MarketplaceEngine.Exceptions
     /// <summary>
     /// Provides extension methods for <see cref="ResourceNotFoundException"/> to facilitate error handling and resource identification.
     /// </summary>
+    /// <remarks>
+    /// The methods in this class generate user‑friendly messages and allow quick identification of a missing resource,
+    /// while also providing a guard clause that throws <see cref="ArgumentNullException"/> when a null exception instance is supplied.
+    /// </remarks>
     public static class ResourceNotFoundExceptionExtensions
     {
         /// <summary>
@@ -13,6 +17,9 @@ namespace MarketplaceEngine.Exceptions
         /// <param name="exception">The exception containing resource information.</param>
         /// <returns>A formatted error message.</returns>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+        /// <remarks>
+        /// Returns a string in the form <c>"Resource {ResourceType} with ID {ResourceId} not found."</c>.
+        /// </remarks>
         public static string GetErrorMessage(this ResourceNotFoundException exception)
         {
             ArgumentNullException.ThrowIfNull(exception);
@@ -25,6 +32,10 @@ namespace MarketplaceEngine.Exceptions
         /// <param name="exception">The exception containing resource information.</param>
         /// <returns>A formatted string in the format "ResourceType - ResourceId".</returns>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+        /// <remarks>
+        /// Returns a string that concatenates <see cref="ResourceNotFoundException.ResourceType"/> and <see cref="ResourceNotFoundException.ResourceId"/>
+        /// separated by a hyphen and spaces, e.g., <c>"Product - 12345"</c>.
+        /// </remarks>
         public static string GetResourceDetails(this ResourceNotFoundException exception)
         {
             ArgumentNullException.ThrowIfNull(exception);
@@ -37,6 +48,9 @@ namespace MarketplaceEngine.Exceptions
         /// <param name="exception">The exception to check.</param>
         /// <returns>True if the exception is a <see cref="ResourceNotFoundException"/>; otherwise, false.</returns>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+        /// <remarks>
+        /// Performs a simple type check using the <c>is</c> operator after validating that <paramref name="exception"/> is not null.
+        /// </remarks>
         public static bool IsResourceNotFound(this Exception exception)
         {
             ArgumentNullException.ThrowIfNull(exception);
