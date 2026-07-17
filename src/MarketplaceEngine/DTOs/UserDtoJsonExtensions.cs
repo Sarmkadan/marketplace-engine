69d7a2c64a05c2ad926ae21378999bd5bc0aa00a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 19:52:42 +0300

    .

--- src/MarketplaceEngine/DTOs/UserDtoJsonExtensions.cs
+++ src/MarketplaceEngine/DTOs/UserDtoJsonExtensions.cs
@@ -13,69 +13,56 @@ namespace MarketplaceEngine.DTOs;
 /// </summary>
 public static class UserDtoJsonExtensions
 {
-    private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-    };
+	private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+	};
 
-    /// <summary>
-    /// Serializes a <see cref="UserDto"/> instance to a JSON string.
-    /// </summary>
-    /// <param name="value">The <see cref="UserDto"/> instance to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the <see cref="UserDto"/>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this UserDto value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Serializes a <see cref="UserDto"/> instance to a JSON string.
+	/// </summary>
+	/// <param name="value">The <see cref="UserDto"/> instance to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the <see cref="UserDto"/>.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	public static string ToJson(this UserDto value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_options) { WriteIndented = true }
-            : _options;
+		var options = indented
+			? new JsonSerializerOptions(_options) { WriteIndented = true }
+			: _options;
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    /// <summary>
-    /// Deserializes a JSON string to a <see cref="UserDto"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A <see cref="UserDto"/> instance, or null if the JSON is null or empty.</returns>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static UserDto? FromJson(string json)
-    {
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
+	/// <summary>
+	/// Deserializes a JSON string to a <see cref="UserDto"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>A <see cref="UserDto"/> instance if deserialization succeeds; otherwise, null.</returns>
+	/// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+	public static UserDto? FromJson(string json)
+		=> string.IsNullOrWhiteSpace(json)
+			? null
+			: JsonSerializer.Deserialize<UserDto>(json, _options);
 
-        return JsonSerializer.Deserialize<UserDto>(json, _options);
-    }
+	/// <summary>
+	/// Attempts to deserialize a JSON string to a <see cref="UserDto"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized <see cref="UserDto"/> instance if successful; otherwise, null.</param>
+	/// <returns>True if deserialization succeeded; otherwise, false.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	public static bool TryFromJson(string json, out UserDto? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="UserDto"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized <see cref="UserDto"/> instance, or null on failure.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out UserDto? value)
-    {
-        value = null;
+		value = string.IsNullOrWhiteSpace(json)
+			? null
+			: JsonSerializer.Deserialize<UserDto>(json, _options);
 
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return false;
-        }
-
-        try
-        {
-            value = JsonSerializer.Deserialize<UserDto>(json, _options);
-            return true;
-        }
-        catch (JsonException)
-        {
-            return false;
-        }
-    }
+		return value is not null;
+	}
 }
