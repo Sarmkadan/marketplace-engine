c4aaf74850beda79b03c31ca81813b5da9cb9204
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Cleanup in RecommendationServiceJsonExtensions

--- src/MarketplaceEngine/Services/RecommendationServiceJsonExtensions.cs
+++ src/MarketplaceEngine/Services/RecommendationServiceJsonExtensions.cs
@@ -26,13 +26,18 @@ public static class RecommendationServiceJsonExtensions
     };
 
     /// <summary>
+    /// Gets the default JSON serialization options for RecommendationService serialization.
+    /// </summary>
+    public static JsonSerializerOptions DefaultOptions => _options;
+
+    /// <summary>
     /// Serializes the <see cref="RecommendationService"/> instance to a JSON string.
     /// </summary>
     /// <param name="value">The service instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON representation of the service instance.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this RecommendationService value, bool indented = false)
+    public static string ToJson(this RecommendationService? value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
 
@@ -48,9 +53,12 @@ public static class RecommendationServiceJsonExtensions
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized service instance, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static RecommendationService? FromJson(string json)
+    public static RecommendationService? FromJson(string? json)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         if (string.IsNullOrWhiteSpace(json))
             return null;
 
@@ -63,9 +71,12 @@ public static class RecommendationServiceJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized service instance, or null if deserialization fails.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out RecommendationService? value)
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static bool TryFromJson(string? json, out RecommendationService? value)
     {
         value = null;
+
+        ArgumentNullException.ThrowIfNull(json);
 
         if (string.IsNullOrWhiteSpace(json))
             return false;
